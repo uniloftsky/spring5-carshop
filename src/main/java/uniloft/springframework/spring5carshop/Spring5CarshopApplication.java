b0625a17667fd9f7a250c2f6402af33dbe746297
uniloftsky <uniloftsky@gmail.com>
2020-09-20 21:47:22 +0300

    Added project

---
+++ src/main/java/uniloft/springframework/spring5carshop/Spring5CarshopApplication.java
@@ -0,0 +1,13 @@
+package uniloft.springframework.spring5carshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CarshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5CarshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uniloft/springframework/spring5carshop/Spring5CarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package uniloft.springframework.spring5carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5CarshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
