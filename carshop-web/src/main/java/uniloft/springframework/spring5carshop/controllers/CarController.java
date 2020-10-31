9456ce97c4e19322e623eb73adf67346eb8e957d
unilofsky <uniloftsky@gmail.com>
2020-10-31 02:12:53 +0200

    Done car details page

--- carshop-web/src/main/java/uniloft/springframework/spring5carshop/controllers/CarController.java
+++ carshop-web/src/main/java/uniloft/springframework/spring5carshop/controllers/CarController.java
@@ -3,8 +3,16 @@ package uniloft.springframework.spring5carshop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import uniloft.springframework.spring5carshop.comparators.CarDescendingComparatorById;
+import uniloft.springframework.spring5carshop.model.Car;
 import uniloft.springframework.spring5carshop.services.CarService;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
@@ -19,5 +27,14 @@ public class CarController {
     public String getCar(@PathVariable Long carId, Model model) {
         model.addAttribute("car", carService.findById(carId));
         return "car/show";
+    }
+
+    @ModelAttribute("recentVehicles")
+    public Set<Car> getRecentVehicles() {
+        Comparator<Car> comparator = new CarDescendingComparatorById();
+        Set<Car> sortedSet = carService.getSortedCars(comparator).stream().limit(4).collect(Collectors.toSet());
+        TreeSet<Car> finalSet = new TreeSet<>(comparator);
+        sortedSet.stream().iterator().forEachRemaining(finalSet::add);
+        return finalSet;
     }
 }
