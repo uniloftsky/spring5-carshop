f7f63f701298fa4e022be6bdc5e3454159576530
unilofsky <uniloftsky@gmail.com>
2020-11-05 20:54:37 +0200

    Started working on admin panel

---
+++ carshop-web/src/main/java/uniloft/springframework/spring5carshop/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package uniloft.springframework.spring5carshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String PASSWORD = "$2y$12$XMx3X9K/XDasBxBt2PRfxO6nINpX0us0KUJ4WzHQjJk/YkLhgmgoO";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/admin").authenticated()
+                    .anyRequest().permitAll()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/admin", true)
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll()
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user").password(PASSWORD).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
